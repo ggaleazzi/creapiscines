b8c228a2f9b0f614d25370b8380864c9e6103856
ggaleazzi <gustavogaleazzi@gmail.com>
2024-06-17 19:09:49 +0200

    fix2

--- app/public/wp-content/uploads/custom-css-js/566.js
+++
@@ -1,8 +0,0 @@
-<!-- start Simple Custom CSS and JS -->
-<script type="text/javascript">
-function fun() {  
-var a = document.getElementById("lightbox");  
-a.className = "shrinkToFit";  
-}  
-</script>
-<!-- end Simple Custom CSS and JS -->
